dd103bda2f4ebeb685f213795a34da01228ec9bc
Aayush <falcon.bridge2000@gmail.com>
2022-01-30 16:15:40 +0530

    used the ui slice to dispatch action from the cart button to toggle the visiblity of cart and rendered the cart accordingly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./store/index";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
