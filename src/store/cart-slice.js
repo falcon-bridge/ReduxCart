e9a4f6b7f1367f0590fbff7649d496e0172efcec
Aayush <falcon.bridge2000@gmail.com>
2022-01-30 19:39:36 +0530

    rendered data dynamically and used the cart slice state to update the badge of cart accordingly on adding item to cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-      //   state.totalQuantity++;
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -27,7 +27,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
-      //   state.totalQuantity--;
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
