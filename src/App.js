7f636b287dc67af8bf8d37f5c6db3399c2901a0f
Aayush <falcon.bridge2000@gmail.com>
2022-01-31 12:17:24 +0530

    kept the transformation logic in reducer only and sent request to firebase ( after the store data is changed ) in the root app component using useEffect() whenever the cart data changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +8,15 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const show = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://using-firebase-ac49a-default-rtdb.firebaseio.com/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {show && <Cart />}
