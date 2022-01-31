836c5e12dadb69ce51b0163d2d61d70e9fc246fa
Aayush <falcon.bridge2000@gmail.com>
2022-01-31 13:47:21 +0530

    shown appropriate noficatiions upon different actions such as adding or removing items in the cart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,91 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { uiActions } from "./store/ui-slice";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const show = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.notification);
   const cart = useSelector((state) => state.cart);
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    fetch(
-      "https://using-firebase-ac49a-default-rtdb.firebaseio.com/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data.",
+        })
+      );
+
+      const response = await fetch(
+        "https://using-firebase-ac49a-default-rtdb.firebaseio.com/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+        // dispatch(
+        //   uiActions.showNotification({
+        //     status: "error",
+        //     title: "Error !",
+        //     message: "Sending cart data failed.",
+        //   })
+        // );
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully.",
+        })
+      );
+    };
+
+    // we don't send data if we open the file for the first time
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error !",
+          message: "Sending cart data failed.",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
+  console.log(notification);
 
   return (
-    <Layout>
-      {show && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {show && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
