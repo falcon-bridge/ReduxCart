af2f7ffb19c41acda49a3c13ea744a8b79c18d9f
Aayush <falcon.bridge2000@gmail.com>
2022-01-31 20:03:42 +0530

    made sure that any extra data that is not needed on the firebase isn't sent there

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -43,7 +43,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://using-firebase-ac49a-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
